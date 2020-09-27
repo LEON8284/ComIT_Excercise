94b9e46a6b2349bafbaf4805d01e2d2e11fcef9d
TAIFUL KHAN <taifulleon@gmail.com>
2020-09-27 18:14:59 -0300

    excercise 4 updated

---
+++ JS/ex_4/index.js
@@ -0,0 +1,8 @@
+alert("Enter two numbers to see if the first is greater than the second");
+
+let firstNumber = prompt("First Number", 100);
+let secondNumber = prompt("Second Number", 100);
+
+
+let result = firstNumber > secondNumber;
+confirm(result);
