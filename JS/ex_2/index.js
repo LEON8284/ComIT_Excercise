3b6d99fb0e280564c4afcc29a8a3e8bc65f3cd27
TAIFUL KHAN <taifulleon@gmail.com>
2020-09-28 13:48:05 -0300

    excercise 2 updated

---
+++ JS/ex_2/index.js
@@ -0,0 +1,19 @@
+const MONTH_OF_BIRTH = "January";
+const firstName = "John";
+const lastName = "Smith";
+
+let city;
+let age;
+let nextCity;
+
+city = "Toronto";
+age = 30;
+nextCity = city;
+
+console.log("MONTH_OF_BIRTH:", MONTH_OF_BIRTH);
+console.log("firstName:", firstName);
+console.log("lastName:", lastName)
+console.log("city:", city);
+console.log("age:", age)
+console.log("nextCity:", nextCity);
+console.log(`${firstName} ${lastName}` );
